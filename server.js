247d1553cfbbf16d89321234854b843c64b05c79
ynaik <ynaik@ebay.com>
2017-05-17 14:29:53 -0700

    changed

--- server.js
+++ server.js
@@ -11,20 +11,25 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 //});
 
-mongoose.connect('mongodb://localhost/restful');
+//mongoose.connect('mongodb://localhost/restful');
 
-var ProductSchema = mongoose.Schema({
-    id : String,
-    name: String,
-    desciption: String
+// var ProductSchema = mongoose.Schema({
+//     id : String,
+//     name: String,
+//     desciption: String
+// });
+
+// var Products = restful.model('products', ProductSchema);
+// Products.methods(['get', 'put', 'post', 'delete']);
+// Products.register(app, '/topics');
+
+app.get("/api/contacts", function(req, res) {
+    res.status(200).json({"id":2});
 });
 
-var Products = restful.model('products', ProductSchema);
-Products.methods(['get', 'put', 'post', 'delete']);
-Products.register(app, '/topics');
-
 //app.listen(3000);
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
